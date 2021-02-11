dcca22ce0518cf50a5904c172284f7387d252c68
Suyeong Rhie <me@euc-kr.net>
2021-02-12 03:14:36 +0900

    first commit

---
+++ namutrimmer.js
@@ -0,0 +1,53 @@
+/**
+ * NamuTrimmer
+ * @author Mirai Kim(Suyeong Rhie) <me@euc-kr.net>
+ * @version 0.1
+ * @license MIT
+ */
+
+function namuTrimmer() {
+    let article = fetchArticle()
+    let strikes = article.getElementsByTagName("del")
+    let links = article.getElementsByTagName("a")
+    trimStrike(strikes)
+    trimRedirectTitle(links)
+}
+
+function trimStrike(strikes) {
+    for (let strike of strikes) {
+        strike.style.visibility = "hidden"
+        strike.style.display = "none"
+    }
+}
+
+function trimRedirectTitle(links) {
+    for (let link of links) {
+        if (link.className === "wiki-link-internal" && !link.text.includes("<img")) {
+            let split = splitRedirect(link)
+            if (!split[1].includes(split[0])) {
+                link.text = split[0] + "(" + split[1] + ")"
+            }
+        }
+    }
+}
+
+function fetchShortAnnotation() {
+
+}
+
+function checkExternalLinkAlive() {
+
+}
+
+function splitRedirect(redirect) {
+    let describeTitle = redirect.text
+    let destinationTitle = decodeURI(redirect.href.replace(redirect.origin + "/w/", ""))
+    return [describeTitle, destinationTitle]
+}
+
+function fetchArticle() {
+    let origin = top.document.getElementsByTagName("article").item(0)
+    return origin.children.item(4).children.item(1).children.item(0).children.item(0)
+}
+
+namuTrimmer()
